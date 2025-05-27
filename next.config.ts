d52856d0ecf4de16332edee26dd6341649aa5952
theredot <theredot2023@gmail.com>
2025-05-27 03:10:48 +0300

    bugFix5

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* existing config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        // You can optionally specify port and pathname if needed
+        // port: '',
+        // pathname: '/account123/**',
+      },
+      // Added configuration for i.imgur.com
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+        // port: '', // Typically not needed for Imgur
+        // pathname: '/**', // Allows any path on i.imgur.com, common for image hosts
+      },
+      // If you have other primary image domains, add their configurations here as well.
+      // For example:
+      // {
+      //   protocol: 'https',
+      //   hostname: 'your-other-image-domain.com',
+      // },
+    ],
+  },
 };
 
 export default nextConfig;
